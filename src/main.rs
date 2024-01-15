43671c5f7c3d6a92d90582b96d4e81119237cfd9
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 16:08:12 +0900

    if mutable reference exists, can't borrow again (error)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     let mut  s = String::from("hello");
 
+    let r1 = &mut s;
+    let r2 = &s;
+    let r3 = &mut s;
+
     change(&mut s);
+
+    println!("{}, {}", r1, r2);
 }
 
 fn change(some_string: &mut String) {
