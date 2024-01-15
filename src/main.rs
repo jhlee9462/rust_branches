52536c4d75e1023565f75570122e36e34c6b3189
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 15:26:22 +0900

    String

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
+    let mut  s  = String::from("hello");
 
-    while index < 5 {
-        println!("The value is: {}", a[index]);
+    s.push_str(", world!");
 
-        index += 1;
-    }
-
-    for element in a {
-        println!("The value is: {element}");
-    }
-
-    for number in (1..4).rev() {
-        println!("{number}!");
-    }
-    println!("LIFTOFF!!!");
+    println!("{}", s);
 }
