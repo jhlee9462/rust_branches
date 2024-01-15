cc71b5ac7025f678d88c5675b7a2ab0fc1f82feb
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 16:33:28 +0900

    string slice

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let s = String::from("hello, world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {}", &word);
 }
 
-fn dangle() -> String {
-    let s = String::from("hello");
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-    s
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
