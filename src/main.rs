4a9bebf51d34047209fa4f00e737c04666761519
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 14:35:56 +0900

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
 
-    while number != 0 {
-        println!("{number}!");
+    while index < 5 {
+        println!("The value is: {}", a[index]);
 
-        number -= 1;
+        index += 1;
     }
 
+    for element in a {
+        println!("The value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
     println!("LIFTOFF!!!");
 }
