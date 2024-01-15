ac11c1ea1d6156abf8d8fe0bdb08f1f8da11f58f
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 16:11:21 +0900

    dangling pointer (error)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 fn main() {
-    let mut  s = String::from("hello");
-
-    let r1 = &mut s;
-    let r2 = &s;
-    let r3 = &mut s;
-
-    change(&mut s);
-
-    println!("{}, {}", r1, r2);
+    let reference_to_nothing = dangle();
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
 }
