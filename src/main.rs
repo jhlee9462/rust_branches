fbf444f33bee2b4082cc52ff33830d857af6a27e
jhlee9462 <jhlee9462@epinet.kr>
2024-01-15 18:03:17 +0900

    struct move

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,13 @@ fn main() {
 
     let user2 = User {
         email: String::from("another@example.com"),
+        // username: String::from("anotherusername"),
         ..user1
     };
+
+    // user2 가 빌리지 않거나, copy가 가능한 것만 사용 가능하다.
+    println!("{}", user1.sign_in_count);
+    println!("{}", user1.email);
 }
 
 struct User {
