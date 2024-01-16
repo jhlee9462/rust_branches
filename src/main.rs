86fd8da16bd9ea336ffb0fb356bb2d7bd7e243bd
jhlee9462 <jhlee9462@epinet.kr>
2024-01-16 09:46:51 +0900

    print struct 3

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        // 표현식을 dbg!에 넣으면 표현식 자체가 출력된다.
+        width: dbg!(30 * scale),
         height: 50,
     };
 
@@ -15,6 +17,9 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    // 표준 에러 콘솔 스트림에 출력하기, 레퍼런스가 아닌 구조체를 전달하면 소유권을 가져가버린다.
+    dbg!(&rect1);
 
     // 구조체 출력하기
     println!("rect1 is {:#?}", rect1);
