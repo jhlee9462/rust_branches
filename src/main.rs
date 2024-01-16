b7f6564f956f1e5a0cbb086b46d9e5ce233d068d
jhlee9462 <jhlee9462@epinet.kr>
2024-01-16 09:05:45 +0900

    Tuple structure

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,7 @@
 fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
-
-    user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        // username: String::from("anotherusername"),
-        ..user1
-    };
-
-    // user2 가 빌리지 않거나, copy가 가능한 것만 사용 가능하다.
-    println!("{}", user1.sign_in_count);
-    println!("{}", user1.email);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
