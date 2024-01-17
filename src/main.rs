d98001b83f9066ff55f1294631698ffef9d2c99b
jhlee9462 <jhlee9462@epinet.kr>
2024-01-17 12:42:10 +0900

    method

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,23 @@ struct Rectangle {
     height: u32,
 }
 
+// 메서드 만들기
+impl Rectangle {
+    // 모든 메서드는 self를 첫 번째 매개변수로 가져야 한다.
+    // 일반 함수처럼 단순히 빌리는 용도, 불변, 가변 모두 가능
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
         // 표현식을 dbg!에 넣으면 표현식 자체가 출력된다.
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-
-    // 표준 에러 콘솔 스트림에 출력하기, 레퍼런스가 아닌 구조체를 전달하면 소유권을 가져가버린다.
-    dbg!(&rect1);
-
-    // 구조체 출력하기
-    println!("rect1 is {:#?}", rect1);
-}
-
-fn area(demensions: &Rectangle) -> u32 {
-    demensions.width * demensions.height
-}
+        rect1.area()
+    ); }
